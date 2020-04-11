daf14ad24d768882ca0dc098e533d1f0b9f95e5a
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2020-04-11 18:57:06 +0530

    User model post developed!!

--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     topic = models.CharField(max_length=200)
     description = models.TextField()
-    author = models.CharField(max_length=50)
     date_created = models.DateTimeField('date published')
 
     def __str__(self):
